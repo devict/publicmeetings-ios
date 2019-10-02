d48c07a16ab4be5fb8ebb27072b957f09497a0ba
swiftcode <mpc@junct.com>
2019-10-01 23:32:35 -0500

    Models
    Add User and Meeting models based on the original design.
    
    Changes to be committed:
    	modified:   publicmeetings-ios.xcodeproj/project.pbxproj
    	new file:   publicmeetings-ios/Models/Meeting.swift
    	new file:   publicmeetings-ios/Models/User.swift

---
+++ publicmeetings-ios/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  publicmeetings-ios
+//
+//  Created by mpc on 10/1/19.
+//  Copyright © 2019 mpc. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let name: String?
+    let email: String?
+    let admin: Bool
+}
