909b4c654e6654b29088b0e3c805fecd70eda6aa
swiftcode <mpc@junct.com>
2019-10-05 13:07:10 -0500

    More Screen
    Stub in a basic More screen with common options.
    
    Changes to be committed:
    	modified:   publicmeetings-ios.xcodeproj/project.pbxproj
    	new file:   publicmeetings-ios/Cells/MoreCell.swift
    	modified:   publicmeetings-ios/Controllers/MeetingsViewController.swift
    	modified:   publicmeetings-ios/Controllers/MoreViewController.swift

--- publicmeetings-ios/Controllers/MoreViewController.swift
+++ publicmeetings-ios/Controllers/MoreViewController.swift
@@ -8,24 +8,66 @@
 
 import UIKit
 
-class MoreViewController: UIViewController {
+class MoreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //MARK: - Properties
+    let moreItems: [String] = ["About","Libraries","Version"]
+
+    var tableView = UITableView()
+    
+    
+    //MARK: - ViewController Delegates
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemTeal
+        
+        setupView()
+        setupLayout()
         setScreenTitle()
-
     }
     
     override func viewWillAppear(_ animated: Bool) {
         setScreenTitle()
     }
     
-    func setScreenTitle() {
+    //MARK: - TableView Delegates
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moreItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "moreCell") as! MoreCell
+        let row = indexPath.row
+        
+        cell.item.text = moreItems[row]
+        return cell
+    }
+    
+    
+    //MARK: - Setup and Layout
+    private func setScreenTitle() {
         DispatchQueue.main.async {
             self.tabBarController?.navigationItem.title = "More"
         }
     }
     
+    private func setupView() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(MoreCell.self, forCellReuseIdentifier: "moreCell")
+        tableView.tableFooterView = UIView()
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 0.0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
