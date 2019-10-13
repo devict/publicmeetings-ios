9e28ac6dff063a7953da3d5bb71b2c22c6788ec0
swiftcode <mpc@junct.com>
2019-10-13 03:20:58 -0500

    Search screen
    Add a basic search screen to the tab bar items.
    
    Fixed an issue with the screen titles not setting properly
    when choosing a screen from the tab bar.
    
    Changes to be committed:
    	modified:   publicmeetings-ios.xcodeproj/project.pbxproj
    	modified:   publicmeetings-ios/Controllers/MinutesViewController.swift
    	new file:   publicmeetings-ios/Controllers/SearchViewController.swift
    	modified:   publicmeetings-ios/TabBar/TabBarController.swift

--- publicmeetings-ios/TabBar/TabBarController.swift
+++ publicmeetings-ios/TabBar/TabBarController.swift
@@ -16,25 +16,29 @@ class TabBarController: UITabBarController {
         //MARK: - Properties
         let meetingsViewController = MeetingsViewController()
         let minutesViewController = MinutesViewController()
+        let searchViewController = SearchViewController()
         let moreViewController = MoreViewController()
          
         //MARK: - TabBar image setup
         let config = UIImage.SymbolConfiguration(weight: .heavy)
         let meetingImage = UIImage(systemName: "calendar", withConfiguration: config)
         let minutesImage = UIImage(systemName: "clock", withConfiguration: config)
+        let searchImage = UIImage(systemName: "magnifyingglass", withConfiguration: config)
         let moreImage = UIImage(systemName: "ellipsis.circle", withConfiguration: config)
         
         meetingsViewController.tabBarItem = UITabBarItem(title: "Meetings", image: meetingImage, selectedImage: meetingImage)
         minutesViewController.tabBarItem = UITabBarItem(title: "Minutes", image: minutesImage, selectedImage: minutesImage)
+        searchViewController.tabBarItem = UITabBarItem(title: "Search", image: searchImage, selectedImage: searchImage)
         moreViewController.tabBarItem = UITabBarItem(title: "More", image: moreImage, selectedImage: moreImage)
         
         //MARK: - Navigation
         meetingsViewController.navigationController?.navigationBar.topItem?.title = "Meetings"
         minutesViewController.navigationController?.navigationBar.topItem?.title = "Minutes"
+        searchViewController.navigationController?.navigationBar.topItem?.title = "Search"
         moreViewController.navigationController?.navigationBar.topItem?.title = "More"
          
         //MARK: - ViewController configuration
-        let viewControllerList = [meetingsViewController, minutesViewController, moreViewController]
+        let viewControllerList = [meetingsViewController, minutesViewController, searchViewController, moreViewController]
         viewControllers = viewControllerList
         
         //MARK: - TabBar configuration
