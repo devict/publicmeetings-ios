eeeb0450a2214568954c72e81947b9ac01f304f8
swiftcode <mpc@junct.com>
2019-12-29 21:14:43 -0600

    Update font size
    Update Standard.systemFontLarge to 50.0
    
    Changes to be committed:
    	modified:   publicmeetings-ios/Constants/StandardValues.swift

--- publicmeetings-ios/Constants/StandardValues.swift
+++ publicmeetings-ios/Constants/StandardValues.swift
@@ -13,7 +13,7 @@ struct Standard {
     static let cornerRadius: CGFloat = 8.0
     static let font: UIFont = UIFont(name: "Damascus", size: 16.0)!
     static let systemFont: UIFont = UIFont.systemFont(ofSize: 16.0)
-    static let systemFontLarge: UIFont = UIFont.systemFont(ofSize: 30.0)
+    static let systemFontLarge: UIFont = UIFont.systemFont(ofSize: 50.0)
     static let fontBold: UIFont = UIFont(name: "DamascusBold", size: 16.0)!
     static let largeFont: UIFont = UIFont(name: "Damascus", size: 40.0)!
 }
